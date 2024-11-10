570bbc845e640dddab55ccc792a7f089024e645a
Espequair <espequair@gmail.com>
2024-11-10 16:29:40 +0100

    [TEST][MOD] test.py : changed so it tests the image dowloading

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from get_files import do_everything, get_cards_json_from_html
+from get_files import do_everything, get_cards_json_from_html, get_images
+from os import chdir
 
 do_everything()
 
 #chdir("workspace")
 #get_cards_json_from_html("cards_html", "cards_catalog.json")
+#get_images()
