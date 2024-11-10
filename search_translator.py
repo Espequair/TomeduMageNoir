80d5f2bd1244867f039958e0655bccc5335c01f8
Espequair <espequair@gmail.com>
2024-11-10 16:42:46 +0100

    [SEARCH][MOD] search_translator.py - Added token type and token

--- search_translator.py
+++ search_translator.py
@@ -1,8 +1,18 @@
 from enum import Enum
+from dataclasses import dataclass
 
+class TOK_TYPE(Enum):
+  PAR_OPEN  = 1 # (
+  PAR_CLOSE = 2 # )
+  QUOTED    = 3 # "Whatever is > in (here)!"
+  OPERAND   = 4 # : ! < >
+  OPERATOR  = 5 # everything else
 
-class TOKEN(Enum):
-  
+@dataclass
+class Token:
+    ttype:      TOK_TYPE
+    content:    str
+
 
 def search(search_string):
   pass
