35b2b24b34aaab93116b1f4e96871966eb10a0af
Espequair <espequair@gmail.com>
2024-11-12 17:02:50 +0100

    [FRONTEND][COLLECTION] added ways to sort, need to implement them in the GUI

--- frontend/src/routes/collection/+page.js
+++ frontend/src/routes/collection/+page.js
@@ -1,11 +1,24 @@
 import { cards } from '../card/cards_catalog.js';
 
+function groupBy(array, func){
+  const result = array.reduce((x, y) => {
+    (x[func(y)] = x[func(y)] || []).push(y);
+    return x;
+  }, {});
+  return result
+};
+
+function sortBy(array, func){
+  return array.toSorted((a, b) => (func(a).localeCompare(func(b))));
+}
+
+function filterBy(array, func){
+  return array.filter(card => func(card));
+}
+
 export function load({ params }) {
-  
-  return {
-    cards: cards.map((card) => ({
-      slug: card.slug,
-      name: card.name
-    }))
-  };
+  const filtered_cards =    filterBy(cards, card=>card.type!="Sort")
+  const sorted_cards =      sortBy(filtered_cards, card => card.name)
+  const grouped_by_cards =  groupBy(sorted_cards, card => card.element)
+  return {cards: grouped_by_cards};
 }
--- frontend/src/routes/collection/+page.svelte
+++ frontend/src/routes/collection/+page.svelte
@@ -1,13 +1,23 @@
 <script>
   let { data } = $props();
+  //console.log(data.cards)
 </script>
 
 <h1> Ma collection </h1>
 
 <ul>
-  {#each data.cards as card}
+{#each Object.entries(data.cards) as [category_name, arr]}
+  <li>{category_name}</li>
+  <ul>
+  {#each arr as card}
     <li>
       <a href="/card/{card.slug}">{card.name}</a>
     </li>
+  {:else}
+    cards is empty
   {/each}
+  </ul>
+{:else}
+categories is empty
+{/each}
 </ul>
