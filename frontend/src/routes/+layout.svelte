c63872a507c112d0cafb6546df89ef9508c0de33
Espequair <espequair@gmail.com>
2024-11-12 08:11:31 +0100

    [FRONTEND] Basics for the homepage of the project

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	let { children } = $props();
+</script>
+
+<h1>Bienvenue sur le Tome du Mage Noir</h1>
+<nav>
+  <a href="/">home</a>
+  <a href="/collection">collection</a>
+</nav>
+
+{@render children()}
