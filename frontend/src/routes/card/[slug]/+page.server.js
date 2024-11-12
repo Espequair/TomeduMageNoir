7fbd8dc9fa5df12a284773171481e8fab2061d7a
Espequair <espequair@gmail.com>
2024-11-12 08:08:27 +0100

    [FRONTEND][CARD] Added the basics to display a card

---
+++ frontend/src/routes/card/[slug]/+page.server.js
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+import { cards } from '../cards_catalog.js';
+
+export function load({ params }) {
+	const card = cards.find((card) => card.slug === params.slug);
+
+	if (!card) error(404);
+  console.log(card)
+
+	return {
+		card
+  };
+};
---
+++ frontend/src/routes/card/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  let {data} = $props();
+  console.log(data.card)
+</script>
+
+<h2>{data.card.name}</h2>
+<p>{data.card.effect}</p>
+<h3>Notes</h3>
+<ol>
+{#each data.card.notes as note, index}
+<li>{note}</li>
+{/each}
+</ol>
