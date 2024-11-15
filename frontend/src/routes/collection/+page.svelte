ae2a246166743f2328203d6855394b7040a23390
Espequair <espequair@gmail.com>
2024-11-15 12:27:16 +0100

    [FRONTEND]messing around with the css to try and get something pretty

--- frontend/src/routes/collection/+page.svelte
+++ frontend/src/routes/collection/+page.svelte
@@ -1,11 +1,27 @@
 <script>
   let { data } = $props();
+  let elements = ["Végétal", "Feu", "Air", "Eau", "Minéral", "Arcane"]
   //console.log(data.cards)
 </script>
+<style>
+@import './styles.css'
+</style>
+
+<nav id="searchbar">
+<div>
+{#each elements as element}
+<div id="button-{element}">
+    <input type="checkbox" class="check">{element}
+</div>
+{/each}
+</div>
+<div>
+</div>
+</nav>
 
 <h1> Ma collection </h1>
 
-<ul>
+<ul id="categories">
 {#each Object.entries(data.cards) as [category_name, arr]}
   <li>{category_name}</li>
   <ul>
