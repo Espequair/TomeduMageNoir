0c98431c21382ec4475190980feafbd4e5a6eba8
Espequair <espequair@gmail.com>
2024-11-15 17:28:41 +0100

    [FRONTEND] Auto indented the collection

--- frontend/src/routes/collection/+page.svelte
+++ frontend/src/routes/collection/+page.svelte
@@ -1,39 +1,39 @@
 <script>
-  let { data } = $props();
-  let elements = ["Végétal", "Feu", "Air", "Eau", "Minéral", "Arcane"]
-  //console.log(data.cards)
+    let { data } = $props();
+    let elements = ["Végétal", "Feu", "Air", "Eau", "Minéral", "Arcane"];
+    //console.log(data.cards)
 </script>
-<style>
-@import './styles.css'
-</style>
 
 <nav id="searchbar">
-<div>
-{#each elements as element}
-<div id="button-{element}">
-    <input type="checkbox" class="check">{element}
-</div>
-{/each}
-</div>
-<div>
-</div>
+    <div>
+        {#each elements as element}
+            <div id="button-{element}">
+                <input type="checkbox" class="check" />{element}
+            </div>
+        {/each}
+    </div>
+    <div></div>
 </nav>
 
-<h1> Ma collection </h1>
+<h1>Ma collection</h1>
 
 <ul id="categories">
-{#each Object.entries(data.cards) as [category_name, arr]}
-  <li>{category_name}</li>
-  <ul>
-  {#each arr as card}
-    <li>
-      <a href="/card/{card.slug}">{card.name}</a>
-    </li>
-  {:else}
-    cards is empty
-  {/each}
-  </ul>
-{:else}
-categories is empty
-{/each}
+    {#each Object.entries(data.cards) as [category_name, arr]}
+        <li>{category_name}</li>
+        <ul>
+            {#each arr as card}
+                <li>
+                    <a href="/card/{card.slug}">{card.name}</a>
+                </li>
+            {:else}
+                category {category_name} is empty
+            {/each}
+        </ul>
+    {:else}
+        categories is empty
+    {/each}
 </ul>
+
+<style>
+    @import "./styles.css";
+</style>
