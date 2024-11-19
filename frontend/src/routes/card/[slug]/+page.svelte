8cb804cd94f92cd34ec40684ebec437f7940dcdf
Espequair <espequair@gmail.com>
2024-11-19 19:03:15 +0100

    [FRONTEND][CARD] more details in the card

--- frontend/src/routes/card/[slug]/+page.svelte
+++ frontend/src/routes/card/[slug]/+page.svelte
@@ -1,13 +1,29 @@
 <script>
   let {data} = $props();
-  console.log(data.card)
+  let card = data.card
 </script>
 
-<h2>{data.card.name}</h2>
-<p>{data.card.effect}</p>
-<h3>Notes</h3>
+<h2>{card.name}</h2>
+<img src="{card.mn_image_link}" alt="{card.name}" width=20%/>
+<h3>{card.element}</h3>
+<h4>Mana Cost</h4>
+<ul>
+    {#each Object.entries(card.mana_cost) as [cost_type, cost_quantity]}
+        <li>{cost_quantity} {cost_type}</li>
+    {/each}
+</ul>
+<h4>Mana Components</h4>
+<ul>
+    {#each Object.entries(card.components) as [cost_type, cost_quantity]}
+        <li>{cost_quantity} {cost_type}</li>
+    {/each}
+</ul>
+<h4>Effects</h4>
+
+<p>{card.effect}</p>
+<h4>Notes</h4>
 <ol>
-{#each data.card.notes as note, index}
+{#each card.notes as note}
 <li>{note}</li>
 {/each}
 </ol>
