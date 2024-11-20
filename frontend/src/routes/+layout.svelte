bc0cdc9bc5026ee1a8f36cb91f5dadbba67d1e4b
Espequair <espequair@gmail.com>
2024-11-20 12:54:19 +0100

    [GITHUB][ACTIONS] changes to make it work

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	let { children } = $props();
+    import { base } from '$app/paths';
 </script>
 
 <h1>Bienvenue sur le Tome du Mage Noir</h1>
 <nav>
-  <a href="/">home</a>
-  <a href="/collection">collection</a>
+  <a href="{base}/">home</a>
+  <a href="{base}/collection">collection</a>
 </nav>
 
 {@render children()}
