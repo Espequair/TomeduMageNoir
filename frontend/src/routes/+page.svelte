d03658225820d222192451b8e591bc57577a47d7
Espequair <espequair@gmail.com>
2024-11-20 17:20:35 +0100

    [FRONTEND][HOME] Added basic info about the project

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,6 @@
 <h1> Home Page</h1>
+
+<p>Le Tome du Mage Noir est un outil de deckbuiding développé par <a href="https://github.com/espequair">Espequair</a> pour le jeu <a href="https://magenoir.com">Mage Noir.</a></p>
+<p>Mage Noir est une propriété de Double Combo Games</p>
+
+<p>Le code est disponible sur <a href="https://github.com/espequair/TomeDuMageNoir">Github</a></p>
