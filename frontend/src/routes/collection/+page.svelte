2b9254f4dda956a324b6cdd29b8ecbd5d3e39ed9
Espequair <espequair@gmail.com>
2024-11-25 17:49:56 +0100

    [FRONTEND][DECKBUILDING] Started to organize the deckbuilding pane

--- frontend/src/routes/collection/+page.svelte
+++ frontend/src/routes/collection/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
     import type { Card } from "./+page.js";
     import CardTable from "./CardTable.svelte";
+    import DeckbuilderContainer from "./DeckbuilderContainer.svelte";
     let { data } = $props();
-    let cards : Card[] = data.cards
+    let cards: Card[] = data.cards;
 </script>
-<CardTable {cards}/>
+
+<div id="useful-container">
+    <CardTable {cards} />
+    <DeckbuilderContainer />
+</div>
+
+<style>
+#useful-container {
+  border: 2px dotted rgb(96 139 168);
+  display: flex;
+  justify-content: space-between;
+}
+
+#useful-container > * {
+  border: 2px solid rgb(96 139 168);
+  border-radius: 5px;
+  background-color: rgb(96 139 168 / 0.2);
+  padding: 1em;
+}
+</style>
---
+++ frontend/src/routes/collection/DeckbuilderContainer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<div id="root-container">
+    Ping
+</div>
+
+<style>
+#root-container {
+    background-color: aliceblue;
+}
+</style>
