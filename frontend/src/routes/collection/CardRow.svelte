48258a2034b6ad8e103ee4fb518aabbe151c86ba
Espequair <espequair@gmail.com>
2024-11-27 08:51:50 +0100

    [FRONTEND][COLLECTION] Show icons

--- frontend/src/routes/collection/CardRow.svelte
+++ frontend/src/routes/collection/CardRow.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-    let {card} = $props();
+    let { card } = $props();
     import { sanitize_element } from "$lib/utils.js";
-    import type { Card } from "./+page.js";
+
+    function insert_icons(str: string): string {
+        const regexMap = {
+            air: /\bair\b/gi,
+            fire: /\bf(ire|eu)\b/gi,
+            mineral: /\bmin(é|e)ral\b/gi,
+            vegetal: /\b(v(égé|ege)tal)\b/gi,
+            arcane: /\b(arcane)\b/gi,
+            water: /\b(eau|water)\b/gi,
+        };
+        Object.entries(regexMap).forEach(([element, regex]) => {
+            const pict_style = "style='height: 1em; background: black; padding:5px;border-radius: 50%;'"
+            str = str.replace(regex,"$&<img class='pict' "+pict_style+" src='/pict/"+element+"_icon.png' alt='$&' />");
+        });
+        return str;
+    }
 </script>
 
 <tr>
@@ -19,21 +34,21 @@
     </td>
 
     <td>
-        {card.element}
+        {@html insert_icons(card.element)}
     </td>
     <td>
         <ul class="noBullets">
-            {#each Object.entries(card.mana_cost) as [a, b]}
+            {#each Object.entries(card.mana_cost).toSorted((f,s)=>(f[0].localeCompare(s[0]))) as [a, b]}
                 <li>
                     {b}
-                    {a}
+                    {@html insert_icons(a)}
                 </li>
             {/each}
         </ul>
     </td>
     <td>
         <ul class="noBullets">
-            {#each Object.entries(card.components) as [a, b]}
+            {#each Object.entries(card.components).toSorted((f,s)=>(f[0].localeCompare(s[0]))) as [a, b]}
                 <li>
                     {b}
                     {a}
@@ -44,11 +59,18 @@
     <td class="preserve-linebreak">
         <b>{card.name}</b>
         <br />
-        {card.effect}
+        {@html insert_icons(card.effect)}
     </td>
 </tr>
-
 <style>
+    .noBullets {
+        list-style-type: none;
+        /* Remove bullets */
+        padding: 0;
+        /* Remove padding */
+        margin: 0;
+        /* Remove margins */
+    }
     td {
         border-left: 1px solid;
     }
