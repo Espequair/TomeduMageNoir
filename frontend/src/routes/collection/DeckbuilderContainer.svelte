1eb60de44cfc77178d67950d1e17ea6427fbe5c1
Espequair <espequair@gmail.com>
2024-11-29 14:53:02 +0100

    [FRONTEND][DECKBUILDING] Deck Adder, Selector, Renamer, Deletor

--- frontend/src/routes/collection/DeckbuilderContainer.svelte
+++ frontend/src/routes/collection/DeckbuilderContainer.svelte
@@ -1,36 +1,45 @@
 <script lang="ts">
     import DeckbuilderRow from "./ DeckbuilderRow.svelte";
     import type { Card } from "./+page.js";
-    import { Deck, activeDeck } from "./shared.svelte.js";
+    import { Deck, decks } from "./shared.svelte.js";
     function pushToClipboard() {
         let exp: string = "";
-        for (let cardTuple of activeDeck.getAllcards()) {
+        for (let cardTuple of decks.activeDeck.getAllcards()) {
             let [card, count]: [Card, number] = cardTuple;
             exp += count + " " + card.name + "\n";
         }
         navigator.clipboard.writeText(exp);
     }
-    
+    function createNewDeck() {
+        decks.push(new Deck("New Deck"));
+        decks.activeDeckNum = decks.decks.length - 1
+    }
 </script>
 
 <div id="root-container">
-    <div id="deck-selector">
-        <select>
+    <div id="deck-selector" style="display: inline;">
+        <button type="button" onclick={createNewDeck}>Create New Deck</button>
+        <select bind:value={decks.activeDeckNum}>
+            {#each decks.list as deck, index}
+                <option value={index}>{index+1} : {deck.name}</option>
+            {/each}
         </select>
+        <input type="text" bind:value={decks.activeDeck.name}>
+        <button type="button" onclick={()=>decks.pop()}>deleteDeck</button>
+
     </div>
     <div id="deck-builder">
         <div id="table-header">
-            <span>Deck : {activeDeck.name}</span>
             <br />
             <button type="button" onclick={pushToClipboard}>
                 Copy to Clipboard
             </button>
             <br />
-            <span>Cards in deck : {activeDeck.getCardCount()}</span>
+            <span>Cards in deck : {decks.activeDeck.getCardCount()}</span>
         </div>
         <table>
             <tbody>
-                {#each activeDeck.getAllcards() as [card, count]}
+                {#each decks.activeDeck.getAllcards() as [card, count]}
                     <tr class="deckbuilder-row">
                         <DeckbuilderRow number={count} {card} />
                     </tr>
